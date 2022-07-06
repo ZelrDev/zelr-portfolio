d896ca938758a84a7a16642cc22e7eb5ff58b0c5
zelrdev <jacob@zelr.me>
2022-07-06 17:13:31 +0100

    code

---
+++ src/pages/api/wg.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="font-['Fira Code'] text-xl flex justify-center items-center overflow-none text-center">
+      <div>
+        {
+          "0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1), -6px -4px 12px 2px #fff"
+        }
+      </div>
+    </div>
+  );
+}
